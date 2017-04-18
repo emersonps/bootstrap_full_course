b45153c1adaf16a41c0c1317f60fb4e39ca3bae9
EMERSON\Emerson <emersonline2007@gmail.com>
2017-04-18 01:24:06 -0300

    Aula 30 - Addons: Layout It BootsWatch BootBoxJs BootstrapNotify Dates

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ $(function(){
 	$("#modal01").modal({backdrop:false, keyboard:false}); //remover a cor do fundo e impedir o uso de ESC
 	
 	$("#modal01").modal("show"); //mostrar o modal ao abrir a página
-
+	
+	
 	setTimeout(function(){
 		$("#modal01").modal("hide"); //fechar o modal automaticamente
 	}, 3000);
@@ -31,5 +32,4 @@ $(function(){
 			btn.button("reset");
 		}, 3000);
 	});
-	
 });
